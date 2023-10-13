5e237a54ebedf6e007c7080ab9270e54e0a2c113
Carlos Morales <110312174+CarlosMorales23@users.noreply.github.com>
2023-10-13 17:27:06 -0300

    Callback Hell

--- src/concepts/02-callbacks.js
+++ src/concepts/02-callbacks.js
@@ -7,20 +7,37 @@ import { heroes } from "../data/heroes"
  */
 export const callbacksComponent = (element) => {
 
-    const id = '5d86371fd55e2e2a30fe1ccb1'
+    const id1 = '5d86371fd55e2e2a30fe1ccb1';
+    const id2 = "5d86371fd55e2e2a30fe1ccb2"
 
-    findHero(id, (hero) => {
+    findHero(id1, (error, hero1) => {
+
+        if ( error) {
+            element.innerHTML = error
+        }
         // element.innerHTML= hero?.name || 'No hay heroe';
-        element.innerHTML= hero?.name;
+
+
+        findHero( id2, (error, hero2)=>{
+            if( error ){
+                element.innerHTML = error;
+                return
+            }
+    
+            element.innerHTML = `${hero1.name} / ${hero2.name}`;
+        })
+
     })
+
+
 
 }
 
 
 /**
  * 
- * @param {String} id 
- * @param { (error?: String, hero: Object) => void} callback 
+ * @param { String } id 
+ * @param { ( error: String|Null, hero: Object)=> void} callback 
  */
 const findHero = (id, callback) =>{
 
@@ -31,6 +48,6 @@ const findHero = (id, callback) =>{
         return; // undefined;
     }
 
-    callback( hero );
+    callback( null, hero );
 }
 
