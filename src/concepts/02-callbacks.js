12d4ae057107e0c4ccf4445f6a3f9636c36ff694
Carlos Morales <110312174+CarlosMorales23@users.noreply.github.com>
2023-10-13 14:35:30 -0300

    NO ES ERROR

--- src/concepts/02-callbacks.js
+++ src/concepts/02-callbacks.js
@@ -10,7 +10,8 @@ export const callbacksComponent = (element) => {
     const id = '5d86371fd55e2e2a30fe1ccb1'
 
     findHero(id, (hero) => {
-        element.innerHTML= hero.name
+        // element.innerHTML= hero?.name || 'No hay heroe';
+        element.innerHTML= hero?.name;
     })
 
 }
@@ -19,10 +20,17 @@ export const callbacksComponent = (element) => {
 /**
  * 
  * @param {String} id 
- * @param { (hero: Object) => void} callback 
+ * @param { (error?: String, hero: Object) => void} callback 
  */
 const findHero = (id, callback) =>{
-    const hero = heroes.find ( hero => hero.id === id)
 
-    callback( hero )
+    const hero = heroes.find ( hero => hero.id === id);
+
+    if ( !hero ) {
+        callback(`Hero with id ${ id } not found`);
+        return; // undefined;
+    }
+
+    callback( hero );
 }
+
